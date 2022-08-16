9aef66201d031e257c85bac13dd2174d8e9621a1
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-16 18:41:01 +0900

    諸々、コミット。

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+import express from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/' , function(req, res){
-  res.send('hello world');
+let app = express();
+let http = createServer(app);
+let io = new Server(http);
+
+app.get('/' , function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('connected');
+    socket.on('message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
 });
 
 http.listen(PORT, function(){
-  console.log('server listening. Port:' + PORT);
+    console.log('server listening. Port:' + PORT);
 });
