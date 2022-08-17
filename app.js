f9dc2a782d8bf4df726f492cecda61690c76e616
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-17 20:41:07 +0900

    不具合を修正。コードを最適化。

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 import express from 'express';
-import { createServer } from "http";
-import { Server } from "socket.io";
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
+const DIR = path.dirname(new URL(import.meta.url).pathname);
 
 let app = express();
 let http = createServer(app);
 let io = new Server(http);
 
 app.get('/' , function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(`${DIR}/index.html`);
 });
 
 io.on('connection', function(socket) {
     console.log('connected');
     socket.on('message', function(msg) {
-        console.log('message: ' + msg);
+        console.log(`message: ${msg}`);
         io.emit('message', msg);
     });
 });
 
 http.listen(PORT, function(){
-    console.log('server listening. Port:' + PORT);
+    console.log(`server listening. Port:${PORT}`);
 });
