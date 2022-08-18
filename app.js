172d48819369715078f3410477662663b8251a4c
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-18 19:08:44 +0900

    CORS対策を実装。設定ファイルの読み込みを実装。

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import path from 'path';
+import config from 'config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || config.get('port');
 const DIR = path.dirname(new URL(import.meta.url).pathname);
-
+const CORS_ORIGIN = config.get('origin');
 let app = express();
 let http = createServer(app);
-let io = new Server(http);
+let io = new Server(http, {
+    cors: {
+        origin: CORS_ORIGIN
+    }
+});
 
 app.get('/' , function(req, res) {
     res.sendFile(`${DIR}/index.html`);
