a1b3d7b43ae2fd2ee11a7f6b8e043ecd6dc4811d
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-19 17:05:26 +0900

    ウェブサーバの機能を削除。

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import path from 'path';
 import config from 'config';
 
 const PORT = process.env.PORT || config.get('port');
-const DIR = path.dirname(new URL(import.meta.url).pathname);
 const CORS_ORIGIN = config.get('origin');
 let app = express();
 let http = createServer(app);
@@ -13,10 +11,6 @@ let io = new Server(http, {
     cors: {
         origin: CORS_ORIGIN
     }
-});
-
-app.get('/' , function(req, res) {
-    res.sendFile(`${DIR}/index.html`);
 });
 
 io.on('connection', function(socket) {
