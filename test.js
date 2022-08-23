fa02339da367fd8bf6b29bbb6f46f78fdbd7d4b5
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-23 19:26:29 +0900

    試しで実装。

---
+++ test.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql2';
+import config from 'config';
+
+const getConnection = () => {
+  let con = mysql.createConnection({
+    host: config.get('db_host'),
+    user: config.get('db_user'),
+    password: config.get('db_password'),
+    database: config.get('db_database')
+  });
+  con.connect();
+
+  return con;
+};
+
+const addMessage = (roomName, userName, message) => {
+  con = getConnection();
+  con.query(
+    'INSERT INTO messages (room_name, user_name, message) VALUES (?, ?, ?)',
+    [roomName, userName, message]
+  );
+  con.end();
+};
